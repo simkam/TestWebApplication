35fb4f3c5236a9882e49b8580114a72d76b5f907
Martin Simka <msimka@redhat.com>
2015-09-02 14:50:04 +0200

    ucp test

--- src/main/java/org/jboss/qa/msimka/testwebapp/TestServlet.java
+++ src/main/java/org/jboss/qa/msimka/testwebapp/TestServlet.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -20,58 +21,31 @@ import java.sql.Statement;
 @WebServlet(name = "TestServlet", urlPatterns = {"/TestServlet"})
 public class TestServlet extends HttpServlet {
 
-    @Resource(lookup = "java:jboss/datasources/TestDatasource")
+    @Resource(lookup = "java:jboss/datasources/OracleUCPDS")
     private DataSource ds;
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse httpServletResponse) throws ServletException, IOException {
-        String qry = "SELECT 1";
+        String qry = "SELECT user FROM dual";
         PrintWriter out = httpServletResponse.getWriter();
         Connection conn1 = null;
-        Connection conn2 = null;
-        Connection conn3 = null;
-        Connection conn4 = null;
-        Connection conn5 = null;
         try {
+            System.out.println("getting connection");
             conn1 = ds.getConnection();
             Statement stmt1 = conn1.createStatement();
-            stmt1.executeQuery(qry);
+            ResultSet rs = stmt1.executeQuery(qry);
+            while(rs.next())
+                out.println(rs.getString(1));
             out.println("conn1 ok");
-
-            conn2 = ds.getConnection();
-            Statement stmt2 = conn2.createStatement();
-            stmt2.executeQuery(qry);
-            out.println("conn2 ok");
-
-            conn3 = ds.getConnection();
-            Statement stmt3 = conn3.createStatement();
-            stmt3.executeQuery(qry);
-            out.println("conn3 ok");
-
-            conn4 = ds.getConnection();
-            Statement stmt4 = conn4.createStatement();
-            stmt4.executeQuery(qry);
-            out.println("conn4 ok");
-
-            conn5 = ds.getConnection();
-            Statement stmt5 = conn5.createStatement();
-            stmt5.executeQuery(qry);
-            out.println("conn4 ok");
         } catch (SQLException e) {
             out.println("fail");
             e.printStackTrace();
         } finally {
             try {
-                if (conn1 != null)
+                if (conn1 != null) {
+                    System.out.println("closing connection");
                     conn1.close();
-                if (conn2 != null)
-                    conn2.close();
-                if (conn3 != null)
-                    conn3.close();
-                if (conn4 != null)
-                    conn4.close();
-                if (conn5 != null)
-                    conn5.close();
+                }
             } catch (SQLException sqle) {
                 sqle.printStackTrace();
             }
